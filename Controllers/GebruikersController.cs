0f3b1731e2daf4d4a4f11927614e8af198d5489a
heise <heisenberg.lex@gmail.com>
2025-10-16 10:15:48 +0200

    create en delete

--- Controllers/GebruikersController.cs
+++ Controllers/GebruikersController.cs
@@ -15,12 +15,40 @@ namespace Flauction.Controllers
             _context = context;
         }
         
-
+        // read
         [HttpGet("api/gebruikers")]
         public async Task<ActionResult<IEnumerable<Gebruiker>>> GetGebruikers()
         {
             return await _context.Gebruikers.ToListAsync();
         }
 
+        // update
+        
+
+        // create
+        [HttpPost("api/gebruikers")]
+        public async Task<ActionResult<Gebruiker>> CreateGebruiker(Gebruiker gebruiker)
+        {
+            _context.Gebruikers.Add(gebruiker);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetGebruikers), new { id = gebruiker.GebruikersID }, gebruiker);
+        }
+
+        // delete
+        [HttpDelete("api/gebruikers/{id}")]
+        public async Task<IActionResult> DeleteGebruiker(int id)
+        {
+            var gebruiker = await _context.Gebruikers.FindAsync(id);
+            if (gebruiker == null)
+            {
+                return NotFound();
+            }
+
+            _context.Gebruikers.Remove(gebruiker);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
